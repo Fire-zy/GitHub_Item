f298ab5fb80ed8e3d852e0f4491613816ac08630
Fire-zy <1604176679@qq.com>
2019-07-30 10:06:08 +0800

    更新

--- user/user.js
+++ user/user.js
@@ -1,5 +1,6 @@
+//user
 var user=new Vue({
-	el:'.user',
+	el:'.update',
 	data:{
 		item:{}
 	},
@@ -25,14 +26,125 @@ var user=new Vue({
 		onFollowingClick(following){
 			window.location.href='../follow/following.html?following=' + following
 		},
-		onEventsClick(login){
-			window.location.href='../events/events.html?login=' + login
-		},
-		onstarredClick(login){
-			window.location.href='../starred/starred.html?login=' + login
-		},
 		onRepoClick(login){
 			window.location.href='../repo/repo.html?login=' + login
 		}
 	}
 })
+var user=new Vue({
+	el:'.header',
+	data:{
+		item:{}
+	},
+	created:function(){
+		var search = location.search
+		var login;
+    	if (search) {
+			login = search.split('=')[1]
+    	}
+		var self=this;
+		fetch('https://api.github.com/users/'+login)
+		.then(function(response){
+			return response.json();
+		})
+		.then(function(myJson){
+			self.item=myJson;
+		});
+	}
+})
+//user结束
+
+
+//events开始
+var events=new Vue({
+	el:'.eventsList',
+	data:{
+		events:[]
+	},
+	created:function(){
+		var search=location.search;
+		var login;
+		if(search){
+			login=search.split('=')[1]
+		}
+		var self=this;
+		fetch('https://api.github.com/users/'+login+'/events')
+		.then(function(response){
+			return response.json();
+		})
+		.then(function(myJson){
+			self.events=myJson;
+		});
+	}
+})
+//events结束
+//starred开始
+var starred=new Vue({
+	el:'.starred',
+	data:{
+		starreds:[]
+	},
+	created:function(){
+		var search = location.search
+		var login;
+    	if (search) {
+			login = search.split('=')[1]
+    	}
+		var self=this;
+		fetch('https://api.github.com/users/'+login+'/starred')
+		.then(function(response){
+			return response.json();
+		})
+		.then(function(myJson){
+			self.starreds=myJson;
+		});
+	}
+})
+//starred结束
+//tab开始
+var imgbox=document.getElementsByClassName("img_box");
+//console.log(imgbox);
+var tabbox=document.getElementsByClassName("tab_box");
+//console.log(tabbox);
+tabbox[0].style.display="block";
+tabbox[1].style.display="none";
+tabbox[2].style.display="none";
+document.getElementById("im0").src="../img/people_pic2.png";
+for(var i=0;i<imgbox.length;i++){
+	imgbox[i].onclick=function(){
+		doTabs(this);
+	}
+}
+function doTabs(obj){
+	if(imgbox[0]==obj){
+		document.getElementById("im0").src="../img/people_pic2.png";
+		document.getElementById("im1").src="../img/activity_pic1.png";
+		document.getElementById("im2").src="../img/star_pic1.png";
+		tabbox[0].style.display="block";
+		tabbox[1].style.display="none";
+		tabbox[2].style.display="none";
+	}else if(imgbox[1]==obj){
+		document.getElementById("im0").src="../img/people_pic1.png";
+		document.getElementById("im1").src="../img/activity_pic2.png";
+		document.getElementById("im2").src="../img/star_pic1.png";
+		tabbox[1].style.display="block";
+		tabbox[0].style.display="none";
+		tabbox[2].style.display="none";
+	}else if(imgbox[2]==obj){
+		document.getElementById("im0").src="../img/people_pic1.png";
+		document.getElementById("im1").src="../img/activity_pic1.png";
+		document.getElementById("im2").src="../img/star_pic2.png";
+		tabbox[2].style.display="block";
+		tabbox[0].style.display="none";
+		tabbox[1].style.display="none";
+	}
+}
+//tab结束
+
+
+
+
+
+
+
+
